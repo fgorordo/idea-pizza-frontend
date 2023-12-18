773072f61bff4dab8afd78a3d213c3eca12e2424
fgorordo <flgorordo@gmail.com>
2023-12-18 10:20:20 -0300

    deleted some console.log for debug

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { RouterProvider } from "react-router-dom"
 
 import { router } from "./router";
 
-import { config } from "./config"
-
 export const App = () => {
-  console.log(config)
   return (
     <RouterProvider  router={router}/>
   )
